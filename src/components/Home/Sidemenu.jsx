205d7ff59f850f7a0814c88ee8a67f05c57fb945
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-05 14:17:01 +0100

    ok

---
+++ src/components/Home/Sidemenu.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Sidemenu() {
+  return <h1>Sidemenu</h1>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
