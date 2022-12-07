4d6a07d6a3eeef90dd987a89c89e2c39a8355b9a
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-07 23:16:30 +0100

    ok

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -42,7 +42,7 @@ export default function Nav() {
         </button>
       </div>
 
-      <Sidemenu />
+      <Sidemenu sidemenuActive={sidemenuActive} />
     </nav>
   );
 }
--- src/components/Home/Sidemenu.jsx
+++ src/components/Home/Sidemenu.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Sidemenu() {
+export default function Sidemenu(props) {
+  const { sidemenuActive } = props;
+  const [showRooms, setShowRooms] = useState(false);
+
+  function ToggleShowRooms() {
+    setShowRooms(!showRooms);
+  }
+
   return (
-    <aside className="hide-sidemenu">
+    <aside className={`${!sidemenuActive ? "hide-sidemenu" : ""}`}>
       <div className="sidemenu-center">
         <ul className="sidemenu-links">
           <li>
             <Link to={"/"}>Home</Link>
           </li>
           <li>
-            <Link to={"/"}>Rooms</Link>
+            <div
+              className={`sidemenu-dropdown ${
+                showRooms ? "show-sidemenu-rooms" : ""
+              }`}
+            >
+              <button
+                onClick={ToggleShowRooms}
+                className="sidemenu-dropdown-btn"
+              >
+                Rooms
+              </button>
+              <ul className="dropdown-content">
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+              </ul>
+            </div>
           </li>
           <li>
             <Link to={"/"}>Contact Us</Link>
