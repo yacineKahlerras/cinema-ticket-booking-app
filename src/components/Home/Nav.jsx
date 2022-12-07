d8df7f29fcb1e16af751d991c17e64c94f23c536
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-07 16:25:46 +0100

    ok

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import downArrow from "../../assets/caret-down-fill.svg";
 
 export default function Nav() {
   const [sidemenuActive, setSidemenuActive] = useState(false);
@@ -9,27 +10,31 @@ export default function Nav() {
 
   return (
     <nav>
-      {/* logo */}
-      <div className="logo"></div>
+      <div className="nav-center">
+        {/* logo */}
+        <div className="logo"></div>
 
-      {/* nav links */}
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Rooms</li>
-        <li>About Us</li>
-      </ul>
+        {/* nav links */}
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>
+            Rooms <img src={downArrow} alt="arrow icon" />
+          </li>
+          <li>About Us</li>
+        </ul>
 
-      {/* hamburger menu btn */}
-      <button onClick={ShowSidemenu} className="hamburger">
-        <div id="nav-icon2" className={`${sidemenuActive ? "open" : ""}`}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </button>
+        {/* hamburger menu btn */}
+        <button onClick={ShowSidemenu} className="hamburger">
+          <div id="nav-icon2" className={`${sidemenuActive ? "open" : ""}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+      </div>
     </nav>
   );
 }
