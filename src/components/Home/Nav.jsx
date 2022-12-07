9852933e594f12bd881c256503911a4025255abb
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-07 15:52:51 +0100

    ok

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Nav() {
-  return <h1>Navigation</h1>;
+  const [sidemenuActive, setSidemenuActive] = useState(false);
+
+  function ShowSidemenu() {
+    setSidemenuActive(!sidemenuActive);
+  }
+
+  return (
+    <nav>
+      {/* logo */}
+      <div className="logo"></div>
+
+      {/* nav links */}
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Rooms</li>
+        <li>About Us</li>
+      </ul>
+
+      {/* hamburger menu btn */}
+      <button onClick={ShowSidemenu} className="hamburger">
+        <div id="nav-icon2" className={`${sidemenuActive ? "open" : ""}`}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+    </nav>
+  );
 }
