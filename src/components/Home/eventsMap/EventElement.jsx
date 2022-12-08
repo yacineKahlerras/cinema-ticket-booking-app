675f8904de6967075bd99b8534a8bc207030b55e
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-08 22:01:01 +0100

    ok

--- src/components/Home/eventsMap/EventElement.jsx
+++ src/components/Home/eventsMap/EventElement.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import { posterLink } from "../../../data";
 
 export default function EventElement(props) {
-  const { imgSrc, title } = props;
+  const { imgSrc, title, language, date, time } = props;
 
   return (
     <div className="event-element-container">
       <div className="poster-container">
         <img src={`${posterLink}${imgSrc}`} alt={title} />
       </div>
-      <div className="text-info-container"></div>
+      <div className="text-info-container">
+        <div>
+          <h2>{title}</h2>
+          <span className="language-text">{language}</span>
+        </div>
+        <div>
+          <span>{date}</span>
+          <span>{time}</span>
+        </div>
+      </div>
     </div>
   );
 }
