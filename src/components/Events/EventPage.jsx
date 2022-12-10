a1ee3865d9e17ca107424707db291768513b0239
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-10 13:09:56 +0100

    ok

--- src/components/Events/EventPage.jsx
+++ src/components/Events/EventPage.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { roomsList } from "../../data";
+import { roomsList, posterLink } from "../../data";
 
 export default function EventPage() {
   const [searchParams] = useSearchParams();
   const roomId = searchParams.get("roomId");
   const subRoomId = searchParams.get("subRoomId");
   const movieId = searchParams.get("movieId");
+
   const room = roomsList.filter((room) => room.id == roomId)[0];
-  console.log(subRoomId);
+  const hasSubRoom = subRoomId !== null;
+  const movie = hasSubRoom
+    ? room.subRooms[subRoomId].movies[movieId]
+    : room.movies[parseInt(movieId)];
+  const { poster, title, date, time, price, langue } = movie;
+
+  const posterImgSrc = `${posterLink}${poster}`;
 
   return (
     <div className="event-page-container">
       <div className="poster-container">
-        <img src="" alt="" />
+        <img src={posterImgSrc} alt="" />
       </div>
       <div className="text-side-container">
-        <h1>title</h1>
+        <h1>{title}</h1>
+        <p>
+          {date} a {time}
+        </p>
+        <p>Language : {langue}</p>
+        <p>price : {price}$</p>
+        <button>Book Ticket</button>
       </div>
     </div>
   );
