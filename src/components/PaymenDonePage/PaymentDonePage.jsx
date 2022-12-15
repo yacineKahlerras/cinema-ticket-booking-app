edf1a99ce55ba7a0e6a3a568527b35bf48585e08
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-15 23:19:59 +0100

    ok

--- src/components/PaymenDonePage/PaymentDonePage.jsx
+++ src/components/PaymenDonePage/PaymentDonePage.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { QRCodeCanvas } from "qrcode.react";
 import { useLocation } from "react-router-dom";
 import { DateDecontructor } from "../Home/eventsMap/EventMothods";
 import Ticket from "./Ticket";
 
-export default function PaymentDonePage(props) {
+export default function PaymentDonePage() {
   const location = useLocation();
   const { title, date, roomTitle, eventTime, bookedSeats } = location.state;
   const tempDate = DateDecontructor(date);
+
+  function Tickets() {
+    return (
+      <div className="tickets-group-container">
+        {bookedSeats.map((seat, index) => {
+          return (
+            <Ticket
+              key={index}
+              location={location}
+              title={title}
+              date={date}
+              roomTitle={roomTitle}
+              eventTime={eventTime}
+              bookedSeats={bookedSeats}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 
   return (
     <div className="payment-completed-container">
@@ -15,17 +34,12 @@ export default function PaymentDonePage(props) {
         <h1>
           Thanks for Purchassing the ticket{bookedSeats.length > 1 ? "s" : ""}
         </h1>
-        <p></p>
+        <p>
+          you can download the tickets here : <button>Download</button>
+        </p>
       </div>
 
-      <Ticket
-        location={location}
-        title={title}
-        date={date}
-        roomTitle={roomTitle}
-        eventTime={eventTime}
-        bookedSeats={bookedSeats}
-      />
+      {Tickets()}
     </div>
   );
 }
