72ac23f423c237ea12aca43b492f278826b803a2
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-29 12:30:08 +0100

    ok

--- src/components/Home/eventsMap/EventElement.jsx
+++ src/components/Home/eventsMap/EventElement.jsx
@@ -1,33 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { posterLink } from "../../../data";
-import { DateDecontructor, LanguageDecontructor } from "./EventMothods";
 
 export default function EventElement(props) {
-  const {
-    movie: { poster, title, langue, date, time },
-  } = props;
-  const { movieId, roomId, subRoomId } = props;
-  const searchParams = `roomId=${roomId}&movieId=${movieId}${
-    subRoomId !== undefined ? `&subRoomId=${subRoomId}` : ""
-  }`;
-
-  const dateDecontructed = DateDecontructor(date);
-  const langueDeconstructed = LanguageDecontructor(langue);
+  const { id, title } = props;
 
   return (
-    <Link className="event-element-link" to={`/EventPage/?${searchParams}`}>
+    <Link className="event-element-link" to={`/EventPage/?${""}`}>
       <div className="event-element-container">
         <div className="poster-container">
-          <span className="language-info">{langueDeconstructed}</span>
-          <img src={`${posterLink}${poster}`} alt={title} />
+          <img src={`${posterLink}/${id}.jpg`} alt={title} />
         </div>
         <div className="text-info-container">
-          <div className="top-text-info">
-            <span>
-              le {dateDecontructed.day}/{dateDecontructed.month} à {time}
-            </span>
-          </div>
           <div className="bottom-text-info">
             <h2>{title}</h2>
           </div>
--- src/components/Home/eventsMap/EventsMap.jsx
+++ src/components/Home/eventsMap/EventsMap.jsx
@@ -1,62 +1,13 @@
 import React, { useContext } from "react";
-import { roomsList } from "../../../data";
-import { FilterParamsContext } from "../Home";
+import { MoviesListContext } from "../../../App";
 import EventElement from "./EventElement";
-import NoEventError from "./NoEventsError";
 
 export default function EventsMap() {
-  const filterParams = useContext(FilterParamsContext);
-  let hasMovies = false;
+  const movieList = useContext(MoviesListContext);
 
-  const elements = roomsList.map((room) => {
-    // if it doesnt match the the wilaya then return nothing
-    if (
-      filterParams.cinema.label.toLowerCase() != "tous" &&
-      filterParams.cinema.label != room.title
-    )
-      return;
-    if (
-      filterParams.wilaya.value != "all" &&
-      filterParams.wilaya.value != room.wilaya
-    )
-      return;
-
-    // if it has subrooms
-    if (room.subRooms) {
-      const subroomMovies = room.subRooms.map((subroom, subRoomIndex) => {
-        const movies = subroom.movies.map((movie, movieIndex) => {
-          return (
-            <EventElement
-              key={movie.title}
-              movie={movie}
-              roomId={room.id}
-              subRoomId={subRoomIndex}
-              movieId={movieIndex}
-            />
-          );
-        });
-        if (subroom.movies.length > 0) hasMovies = true;
-        return [...movies];
-      });
-      return [...subroomMovies];
-    }
-
-    // if its not a subroom
-    const roomMovies = room.movies.map((movie, index) => {
-      return (
-        <EventElement
-          key={movie.title}
-          movie={movie}
-          roomId={room.id}
-          movieId={index}
-        />
-      );
-    });
-    if (room.movies.length > 0) hasMovies = true;
-    return [...roomMovies];
+  const movieElements = movieList.list.map((movie, index) => {
+    return <EventElement key={index} id={movie.id} title={movie.title} />;
   });
 
-  return (
-    <div className="events-map">{hasMovies ? elements : <NoEventError />}</div>
-  );
+  return <div className="events-map">{movieElements}</div>;
 }
