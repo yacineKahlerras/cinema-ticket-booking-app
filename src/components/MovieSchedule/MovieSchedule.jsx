d06dd4e318f67e3bdb27243c162a9d941cc70d8f
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-12-30 17:39:41 +0100

    ok

--- src/components/MovieSchedule/MovieSchedule.jsx
+++ src/components/MovieSchedule/MovieSchedule.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { movieSchedule } from "../../data";
 import { getMovieSchedule } from "../../firebase/dataFetcher";
 
 export function SchedulePageLoader({ params }) {
@@ -9,7 +10,7 @@ export function SchedulePageLoader({ params }) {
 export default function MovieSchedule() {
   const linkParams = useLoaderData();
   const id = linkParams.match(/(\w+)/g)[0];
-  const [schedule, setSchedule] = useState();
+  const [schedule, setSchedule] = useState(movieSchedule);
 
   useEffect(() => {
     // getMovieSchedule(id);
