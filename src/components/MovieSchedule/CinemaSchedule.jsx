e665579148208efc95463ef2f7fcf046ab30de6a
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-02 09:47:52 +0100

    ok

--- src/components/MovieSchedule/CinemaSchedule.jsx
+++ src/components/MovieSchedule/CinemaSchedule.jsx
@@ -6,12 +6,17 @@ export default function CinemaSchedule(props) {
   function DatesMapElements() {
     return cinemaSchedule.dates.map((dateInfo, index) => {
       const dateObject = new Date(dateInfo.date.seconds * 1000);
+      const month = dateObject.toDateString().match(/(\w+)/g)[1];
+      const day = dateObject.toDateString().match(/(\w+)/g)[2];
+      const hour = dateObject.getHours();
       const language = dateInfo.language;
 
       return (
         <div key={index} className="date-container">
-          <span className="date">25 Jan</span>
-          <span className="hour">19h</span>
+          <span className="date">
+            {day} {month}
+          </span>
+          <span className="hour">{hour}h</span>
           <span className="language">{language}</span>
         </div>
       );
