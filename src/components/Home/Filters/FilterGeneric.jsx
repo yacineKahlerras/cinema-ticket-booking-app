678faf8742731cd47ee0b076509b45e12f011b85
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-05 23:45:17 +0100

    ok

--- src/components/Home/Filters/FilterGeneric.jsx
+++ src/components/Home/Filters/FilterGeneric.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-import Select from "react-select";
+import Select, { components } from "react-select";
+import { Component } from "react";
 
 export default function FilterGeneric(props) {
-  const { filterLabel, options, changeHandler, paymentMethod } = props;
+  let { filterLabel, options, changeHandler, paymentMethod } = props;
+
+  options = [
+    { value: "England", label: "England", icon: "england.svg" },
+    { value: "Germany", label: "Germany", icon: "germany.svg" },
+  ];
 
   return (
     <div className="filter-container">
       <div className="filter">
         <span className="filter-label">{filterLabel}</span>
         <Select
-          value={paymentMethod}
-          onChange={changeHandler}
+          // value={paymentMethod}
+          // onChange={changeHandler}
           options={options}
-          defaultValue={paymentMethod}
+          // defaultValue={paymentMethod}
         />
       </div>
     </div>
   );
 }
+
+const { Option } = components;
+const IconOption = (props) => (
+  <Option {...props}>
+    <img
+      src={require("./" + props.data.icon)}
+      style={{ width: 36 }}
+      alt={props.data.label}
+    />
+    {props.data.label}
+  </Option>
+);
