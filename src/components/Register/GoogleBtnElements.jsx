e202d30532e5a16a763c008e37ff4a51cdf8c822
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-07 23:15:27 +0100

    ok

--- src/components/Register/GoogleBtnElements.jsx
+++ src/components/Register/GoogleBtnElements.jsx
@@ -5,7 +5,7 @@ import LoadingSpinner from "../Nav/LoadingSpinner";
 
 export default function GoogleBtnElements(props) {
   const { user } = props;
-  const hasUser = user != null && user.displayName != null;
+  const hasUser = user != null && user.email != null;
   const isSignInLoading = localStorage.getItem("isSignInLoading");
 
   const googleBtn = (
@@ -16,7 +16,7 @@ export default function GoogleBtnElements(props) {
     />
   );
 
-  const userInfoElement = user != null ?? (
+  const userInfoElement = hasUser ? (
     <h3 className="user-info">
       {user.email}{" "}
       <svg
@@ -29,6 +29,8 @@ export default function GoogleBtnElements(props) {
         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
       </svg>
     </h3>
+  ) : (
+    ""
   );
 
   return hasUser ? (
