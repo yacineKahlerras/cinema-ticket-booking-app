8f22505383449d34b6ef1298fc03516a5a6b0a41
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-13 14:42:11 +0100

    released all the database fetches

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,12 +6,12 @@ import "./style/_index.scss";
 
 export default function Dashboard() {
   const { user } = useContext(UserContext);
-  const [userTicketsList, setUserTicketsList] = useState(userTicketsListObject);
+  const [userTicketsList, setUserTicketsList] = useState({});
   const [ticketElement, setTicketElement] = useState([]);
 
   useEffect(() => {
-    GetUserTickets(user.uid, setUserTicketsList);
-  }, []);
+    if (user.uid) GetUserTickets(user.uid, setUserTicketsList);
+  }, [user]);
 
   if (!ticketElement.length) {
     for (const movieKey in userTicketsList) {
@@ -32,50 +32,13 @@ export default function Dashboard() {
   return (
     <div className="dashboard-container">
       <h1>Mes Tickets</h1>
-      <div className="reserved-tickets-map">{ticketElement}</div>
+      <div className="reserved-tickets-map">
+        {ticketElement.length ? (
+          ticketElement
+        ) : (
+          <p>vous n'avez pas encore achetez de tickets.</p>
+        )}
+      </div>
     </div>
   );
 }
-
-const userTicketsListObject = {
-  882598: [
-    {
-      dateParts: {
-        day: 7,
-        month: "Janvier",
-        dayOfWeek: "samedi",
-        hour: 18,
-        year: 2023,
-      },
-      cinemaName: "ibn khaldoun",
-      seat: {
-        col: 5,
-        row: 7,
-      },
-      movieInfo: {
-        price: 800,
-        title: "smile",
-        id: "882598",
-      },
-    },
-    {
-      movieInfo: {
-        id: "882598",
-        price: 800,
-        title: "smile",
-      },
-      seat: {
-        row: 7,
-        col: 6,
-      },
-      cinemaName: "ibn khaldoun",
-      dateParts: {
-        month: "Janvier",
-        day: 7,
-        dayOfWeek: "samedi",
-        hour: 18,
-        year: 2023,
-      },
-    },
-  ],
-};
